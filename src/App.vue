45ba2772da1aaab3f1bac89f33e78fd81efc51bf
Somewell <samvel121202@mail.ru>
2022-08-13 22:15:33 +0300

    Html, tailwind, api

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,169 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
+        <!--        <div class="fixed w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center">-->
+        <!--            <svg class="animate-spin -ml-1 mr-3 h-12 w-12 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"-->
+        <!--                 viewBox="0 0 24 24">-->
+        <!--                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>-->
+        <!--                <path class="opacity-75" fill="currentColor"-->
+        <!--                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>-->
+        <!--            </svg>-->
+        <!--        </div>-->
+        <div class="container">
+            <section>
+                <div class="flex">
+                    <div class="max-w-xs">
+                        <label for="wallet" class="block text-sm font-medium text-gray-700"
+                        >Тикер</label
+                        >
+                        <div class="mt-1 relative rounded-md shadow-md">
+                            <input
+                                v-model="ticker"
+                                @keydown.enter="addTicker"
+                                type="text"
+                                name="wallet"
+                                id="wallet"
+                                class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
+                                placeholder="Например DOGE"
+                            />
+                        </div>
+                        <div class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap">
+            <span
+                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+              BTC
+            </span>
+                            <span
+                                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+              DOGE
+            </span>
+                            <span
+                                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+              BCH
+            </span>
+                            <span
+                                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+              CHD
+            </span>
+                        </div>
+                        <div class="text-sm text-red-600">Такой тикер уже добавлен</div>
+                    </div>
+                </div>
+                <button
+                    @click="addTicker"
+                    type="button"
+                    class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+                >
+                    <add-icon/>
+                    Добавить
+                </button>
+            </section>
+
+            <template v-if="tickers.length">
+                <hr class="w-full border-t border-gray-600 my-4"/>
+                <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+                    <div
+                        v-for="(t, i) in tickers"
+                        :key="i"
+                        @click="selectTicker(t)"
+                        class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
+                        :class="{ 'border-4': sel === t }"
+                    >
+                        <div class="px-4 py-5 sm:p-6 text-center">
+                            <dt class="text-sm font-medium text-gray-500 truncate">
+                                {{ t.name }} - USD
+                            </dt>
+                            <dd class="mt-1 text-3xl font-semibold text-gray-900">
+                                {{ t.price }}
+                            </dd>
+                        </div>
+                        <div class="w-full border-t border-gray-200"></div>
+                        <button
+                            @click.stop="deleteTicker(t)"
+                            class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
+                        >
+                            <delete-icon/>
+                            Удалить
+                        </button>
+                    </div>
+                </dl>
+            </template>
+            <template v-if="sel">
+                <hr class="w-full border-t border-gray-600 my-4"/>
+                <section class="relative">
+                    <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
+                        {{ sel.name }} - USD
+                    </h3>
+                    <div class="flex items-end border-gray-600 border-b border-l h-64">
+                        <div
+                            v-for="(bar, i) in normalizeGraph()"
+                            :key="i"
+                            :style="{ height: `${bar}%`}"
+                            class="bg-purple-800 border w-10"
+                        ></div>
+                    </div>
+                    <button
+                        type="button"
+                        class="absolute top-0 right-0"
+                        @click="clearSel"
+                    >
+                        <close-icon/>
+                    </button>
+                </section>
+            </template>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CloseIcon from "@/assets/img/icons/CloseIcon";
+import DeleteIcon from "@/assets/img/icons/DeleteIcon";
+import AddIcon from "@/assets/img/icons/AddIcon";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {AddIcon, DeleteIcon, CloseIcon},
+    data() {
+        return {
+            ticker: "",
+            tickers: [],
+            sel: null,
+            graph: []
+        }
+    },
+
+    methods: {
+        addTicker() {
+            const currentTicker = {name: this.ticker, price: "-"};
+            this.tickers.push(currentTicker);
+            setInterval(async () => {
+                const f = await fetch(
+                    `https://min-api.cryptocompare.com/data/price?fsym=${currentTicker.name}&tsyms=USD&api_key=2bb59c3cba8de19675c26a6a65a1c298b555117ead02f242a572e2b7b0ae2516`
+                );
+                const data = await f.json();
+                this.tickers.find(t => t.name === currentTicker.name).price = data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
+
+                if (this.sel?.name === currentTicker.name) {
+                    this.graph.push(data.USD);
+                }
+            }, 3000)
+            this.ticker = "";
+        },
+        deleteTicker(tickerToRemove) {
+            this.tickers = this.tickers.filter(t => t !== tickerToRemove);
+        },
+        selectTicker(tickerToSelect) {
+            this.sel = tickerToSelect;
+            this.graph = [];
+        },
+        clearSel() {
+            this.sel = null
+        },
+        normalizeGraph() {
+            const max = Math.max(...this.graph);
+            const min = Math.min(...this.graph);
+            return this.graph.map(
+                bar => 5 + ((bar - min) * 95) / (max - min)
+            );
+        }
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
